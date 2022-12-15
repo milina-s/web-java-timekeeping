5d0803cf9db14519d3bae080bb29eb495115d189
milina-s <samokhatnyaya.milina@gmail.com>
2022-12-15 17:50:14 +0200

    feat: add lombok to entities fix: change foreign keys

--- src/main/java/com/example/entities/Category.java
+++ src/main/java/com/example/entities/Category.java
@@ -1,54 +1,13 @@
 package com.example.entities;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class Category {
 
     private Long id;
     private String name;
 
-    public Category() {
-    }
-
-    public Category(CategoryBuilder builder) {
-        this.id = builder.id;
-        this.name = builder.name;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public static class CategoryBuilder {
-
-        private Long id;
-        private String name;
-
-        public CategoryBuilder() {
-        }
-
-        public CategoryBuilder setId(Long id) {
-            this.id = id;
-            return this;
-        }
-
-        public CategoryBuilder setName(String name) {
-            this.name = name;
-            return this;
-        }
-
-        public Category build() {
-            return new Category(this);
-        }
-    }
 }
--- src/main/java/com/example/entities/User.java
+++ src/main/java/com/example/entities/User.java
@@ -1,5 +1,10 @@
 package com.example.entities;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class User {
 
     private Long id;
@@ -9,110 +14,4 @@ public class User {
     private String password;
     private UserRole role;
 
-    public User() {
-    }
-
-    public User(UserBuilder builder) {
-        this.id = builder.id;
-        this.name = builder.name;
-        this.lastname = builder.lastname;
-        this.email = builder.email;
-        this.password = builder.password;
-        this.role = builder.role;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getLastname() {
-        return lastname;
-    }
-
-    public void setLastname(String lastname) {
-        this.lastname = lastname;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public UserRole getRole() {
-        return role;
-    }
-
-    public void setRole(UserRole role) {
-        this.role = role;
-    }
-
-    public static class UserBuilder {
-
-        private Long id;
-        private String name;
-        private String lastname;
-        private String email;
-        private String password;
-        private UserRole role;
-
-        public UserBuilder() {
-        }
-
-        public UserBuilder setId(Long id) {
-            this.id = id;
-            return this;
-        }
-
-        public UserBuilder setName(String name) {
-            this.name = name;
-            return this;
-        }
-
-        public UserBuilder setLastname(String lastname) {
-            this.lastname = lastname;
-            return this;
-        }
-
-        public UserBuilder setEmail(String email) {
-            this.email = email;
-            return this;
-        }
-
-        public UserBuilder setPassword(String password) {
-            this.password = password;
-            return this;
-        }
-
-        public UserBuilder setRole(UserRole role) {
-            this.role = role;
-            return this;
-        }
-
-        public User build () {
-            return new User(this);
-        }
-    }
 }
